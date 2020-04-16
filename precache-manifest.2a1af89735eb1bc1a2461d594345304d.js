fdc306d5a16ce9ba83b897491fea0143d32a4343
codeIriss <goal.matters.20@gmail.com>
2020-04-16 14:02:27 +0530

    Updates

---
+++ precache-manifest.2a1af89735eb1bc1a2461d594345304d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aff6e1354534f22fffa1dcf388793c8",
+    "url": "/Weather-Dev/index.html"
+  },
+  {
+    "revision": "064792aea3f49a11ac5f",
+    "url": "/Weather-Dev/static/css/main.b7d5da85.chunk.css"
+  },
+  {
+    "revision": "b17fc90a4dd13d33be5c",
+    "url": "/Weather-Dev/static/js/2.b2556b53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Dev/static/js/2.b2556b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064792aea3f49a11ac5f",
+    "url": "/Weather-Dev/static/js/main.d3a0d80c.chunk.js"
+  },
+  {
+    "revision": "b9f2a6f3dacdb5058428",
+    "url": "/Weather-Dev/static/js/runtime-main.103823dc.js"
+  },
+  {
+    "revision": "a5b5a76c10d240d8f36775c1872adc7f",
+    "url": "/Weather-Dev/static/media/cold3.a5b5a76c.jpg"
+  },
+  {
+    "revision": "38fa5ec534cb7dc45750dd97c4b95d1a",
+    "url": "/Weather-Dev/static/media/warm1.38fa5ec5.jpg"
+  }
+]);
